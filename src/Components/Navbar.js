3bbb1ae37652e183bbecb327197e9a00cbcebaa4
teddywests1de <dirkieys21@gmail.com>
2024-09-20 15:02:23 +0200

    Adding Navbar & Hero Sections

---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Logo from "./Logo";
+
+function Navbar() {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    function handleOpenMenu() {
+       setOpenMenu(!openMenu);
+    }
+    return(
+    
+    <>
+    <header className="App-header">
+       <Logo/>
+        <nav className="menu">
+            <ul className={openMenu ? 'primary-nav closed' : 'primary-nav'}>
+                <li><a href="/#">Home</a></li>
+                <li><a href="/#">Our Story</a></li>
+                <li><a href="/#">Venue</a></li>
+                <li><a href="/#">RSVP</a></li>
+                <li><a href="/#">Gallery</a></li>
+                <button className={openMenu ? 'btn btn-close' : 'btn open btn-close'} onClick={handleOpenMenu}><i className="fa-solid fa-xmark"></i></button>
+            </ul>
+            <button className={openMenu ? 'btn open btn-open' : 'btn btn-open'} onClick={handleOpenMenu}><i className="fa-solid fa-bars"></i></button>
+        </nav>
+        
+    </header>
+    <h1 className="main-title">Our Happily Ever After</h1>
+    <p className="lead-text">Our lovestory began on a sunny day in the park. Where we first laid eyes on each other.</p>
+    </>
+    )
+}
+
+export default Navbar;
---
+++ src/Components/Venues.js
@@ -0,0 +1,16 @@
+function Venues() {
+    return (
+        <div>
+            <h2>
+                Venues
+            </h2>
+            <p>
+            **Venue & Time**  
+            Join us for our special day at the stunning Doornkraal, nestled in the heart of Riversdale. The ceremony will begin at 12:00 AM, followed by a reception full of joy, laughter, and celebration. Our venue, known for its beautiful surroundings and elegant ambiance, provides the perfect backdrop for this unforgettable occasion. Please arrive a few minutes early to ensure a seamless start to the festivities. We can’t wait to share this moment with you in such a beautiful setting!
+            </p>
+            <button className="btn-primary">RSVP</button>
+        </div>
+    )
+}
+
+export default Venues
